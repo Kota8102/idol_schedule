1a52cefd125fec795a50d9fddd768b154724885e
Kota <pontan0801.konta@gmail.com>
2023-01-21 16:30:05 +0900

    404ページの実装 #20

--- src/page/404.tsx
+++ src/page/404.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
 
 import Layout from '../component/organisms/layout';
 
 const NotFound: React.FC = () => {
-    const location = useLocation();
 
     return (
         <Layout>
-            <div className="bg-red-500 text-white text-center py-12">
-            <h1 className="text-6xl">404</h1>
-            <p className="text-xl">{location.pathname}は見つかりませんでした。</p>
-        </div>
+            <section className="flex items-center h-full p-16 dark:bg-gray-900 dark:text-gray-100">
+                <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
+                    <div className="max-w-md text-center">
+                        <h2 className="mb-8 font-extrabold text-9xl dark:text-gray-600">
+                            <span className="sr-only">Error</span>404
+                        </h2>
+                        <p className="text-2xl font-semibold md:text-3xl">Sorry, we couldn't find this page.</p>
+                        <p className="mt-4 mb-8 dark:text-gray-400">But dont worry, you can find plenty of other things on our homepage.</p>
+                        <a rel="noopener noreferrer" href="/" className="px-8 py-3 font-semibold rounded bg-gray-800 hover:bg-gray-500 text-white dark:bg-violet-400 dark:text-gray-900">Back to homepage</a>
+                    </div>
+                </div>
+            </section>
         </Layout>
         
     );
