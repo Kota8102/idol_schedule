b9fd31d389b222121f185b8e741c3db9b016f28a
Kota <pontan0801.konta@gmail.com>
2023-01-21 15:10:56 +0900

    プライバシーポリシーの実装 #15

--- src/route/route.tsx
+++ src/route/route.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../page/home";
 import AboutPage from '../page/about';
 import Idols from '../page/idols';
-import Privacy from '../page/privacy';
+import PrivacyPage from '../page/privacy';
 import NotFound from '../page/404';
 // import ContactPage from '../page/contact';
 
@@ -16,7 +16,7 @@ const RouteManager:React.FC=() => {
           <Route index element={<Home />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/idols" element={<Idols />} />
-          <Route path="/privacypolicy" element={<Privacy />} />
+          <Route path="/privacypolicy" element={<PrivacyPage />} />
           {/* <Route path="/contact" element={<ContactPage />} /> */}
           <Route path="*" element={<NotFound />} />
         </Routes>
