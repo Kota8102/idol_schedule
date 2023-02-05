6f2ad6416de9331b3be95214c16c1408103d7c17
Kota <pontan0801.konta@gmail.com>
2023-02-05 14:10:22 +0900

    モーダルの実装 #12

---
+++ src/component/atom/createLink.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function CreateLink(text: string) {
+    return text.split("\n").map((line, i) => {
+      if (line.startsWith("http")) {
+        return (
+          <a key={i} href={line} target="_blank" rel="noopener noreferrer">
+            {line}
+          </a>
+        );
+      } else {
+        return <span key={i}>{line}</span>;
+      }
+    });
+  }
+
+
+export default CreateLink
