16e37e921f90707248d0de51c040b17c967bba7a
Kota <pontan0801.konta@gmail.com>
2023-03-17 00:38:31 +0900

    フォント変更 #35

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import reportWebVitals from './utils/reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import reportWebVitals from './utils/reportWebVitals'
 
-import "./styles/style.sass"
-import RouteManager from './route/route';
+import './styles/style.sass'
+import RouteManager from './route/route'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <RouteManager />
-);
+import '@fontsource/ibm-plex-sans-jp'
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(<RouteManager />)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
