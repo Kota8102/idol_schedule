0deb243cc68570cad91aad9b2fe7093fb065802f
Kota <pontan0801.konta@gmail.com>
2023-03-21 11:04:25 +0900

    adsenseの追加

--- src/component/organisms/layout.tsx
+++ src/component/organisms/layout.tsx
@@ -1,4 +1,5 @@
 import React, { ReactNode } from 'react'
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 
 import Header from './header'
 import Footer from './footer'
@@ -14,18 +15,27 @@ type Props = {
 const Layout = ({ children, isIdolslist }: Props) => {
 	return (
 		<AppContext>
-			<div className="flex flex-col h-screen">
-				<Header />
-				<main className="flex-grow py-1 md:p-2">
-					<div className="flex flex-col md:flex-row">
-						<div className="basis-1/12 p-4 md:inline-block">
-							<Sidebar isIdolslist={isIdolslist} />
+			<HelmetProvider>
+				<Helmet>
+					<script
+						async
+						src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3246657715738995"
+						crossOrigin="anonymous"
+					></script>
+				</Helmet>
+				<div className="flex flex-col h-screen">
+					<Header />
+					<main className="flex-grow py-1 md:p-2">
+						<div className="flex flex-col md:flex-row">
+							<div className="basis-1/12 p-4 md:inline-block">
+								<Sidebar isIdolslist={isIdolslist} />
+							</div>
+							<div className="flex-auto md:p-1">{children}</div>
 						</div>
-						<div className="flex-auto md:p-1">{children}</div>
-					</div>
-				</main>
-				<Footer />
-			</div>
+					</main>
+					<Footer />
+				</div>
+			</HelmetProvider>
 		</AppContext>
 	)
 }
