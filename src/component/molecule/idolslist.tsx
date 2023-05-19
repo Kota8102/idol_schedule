df08f82eeabe5e483d1d58d74f8bf20fa689a651
Kota <pontan0801.konta@gmail.com>
2023-05-19 09:42:55 +0000

    チェックボックスの色が反映されない Fixes #56

--- src/component/molecule/idolslist.tsx
+++ src/component/molecule/idolslist.tsx
@@ -6,10 +6,12 @@ import Toggle from '../atom/toggle'
 type Props = {
 	name: string
 	color: string
+	colorcode: string
 	id: number
 }
 
 const IdolsList: React.FC<{ idols: Props[] }> = ({ idols }) => {
+	console.log(idols)
 	return (
 		<div className="mt-1 flex flex-col md:mt-16">
 			<h2 className="ml-1 text-xl font-medium">Idol List</h2>
@@ -18,7 +20,8 @@ const IdolsList: React.FC<{ idols: Props[] }> = ({ idols }) => {
 				{idols.map((idol, index) => (
 					<Checkbox
 						key={index}
-						inp_name={idol.name}
+						name={idol.name}
+						colorcode={idol.colorcode}
 						color={idol.color}
 						index={index}
 						groupid={idol.id.toString()}
